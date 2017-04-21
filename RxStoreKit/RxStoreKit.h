559e9d280070c1462c9b8622dccb3112a2aa3196
marcbaldwin <marc.baldwin88@gmail.com>
2017-04-21 17:23:03 +0100

    Initial Commit

---
+++ RxStoreKit/RxStoreKit.h
@@ -0,0 +1,19 @@
+//
+//  RxStoreKit.h
+//  RxStoreKit
+//
+//  Created by Marc Baldwin on 21/04/2017.
+//  Copyright © 2017 Marc Baldwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxStoreKit.
+FOUNDATION_EXPORT double RxStoreKitVersionNumber;
+
+//! Project version string for RxStoreKit.
+FOUNDATION_EXPORT const unsigned char RxStoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxStoreKit/PublicHeader.h>
+
+
